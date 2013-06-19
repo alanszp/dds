870b9cc60a9cc72d2a6c1e6fef726c46109a5e21
Alan Szp <alanszp@gmail.com>
2013-06-18 22:40:22 -0300

    Refactor name Combo -> ComboEntrada + Fixes

--- src/recital/ComboEntrada.scala
+++ src/recital/ComboEntrada.scala
@@ -1,14 +1,15 @@
 package recital
 
-class Combo (porcentajeDescuento: Double) {
+class ComboEntrada (var porcentajeDescuento: Double) {
   
   var entradas: Set[Entrada] = Set()
   
   def precioAux =  entradas.map(_.precioEntrada).sum
   
-  def precioEntrada ={
+  def precioEntrada : Double ={
       if(this.precioAux > 1000){
-    	  precioAux * porcentajeDescuento
+    	  return this.precioAux * porcentajeDescuento
       }
+      	this.precioAux
   }
 }
