e684b51d08ffdcb488430c296b4fbe2a8a3b8a35
tomasgropper <tomasgropper@gmail.com>
2013-08-26 11:37:34 -0300

    Agrego a Producto las variables para trabajr con los manejadores

--- ManejoStock/src/com/utn/comun/Producto.scala
+++ ManejoStock/src/com/utn/comun/Producto.scala
@@ -3,7 +3,7 @@ package com.utn.comun
 import org.scalatest.junit._
 import org.junit.Assert._
 
-class Producto(override val nombre:String, val cantMinima: Int,val cantMaxima : Int, val puntoDePedido : Int) extends ComponenteCompuesto(nombre) {
+class Producto(override val nombre:String, val cantMinima: Int,val cantMaxima : Int, val puntoDePedido : Int, val necesitaLog: Boolean, val cantCriticas: Int) extends ComponenteCompuesto(nombre) {
   
   //val cantMinima  : Int
   
