d77170f71c0d35697e11ee8760a6f2a27630c3d0
gkuz <guidokuz@gmail.com>
2013-08-26 12:52:35 -0300

    Eventos salida

--- ManejoStock/src/com/utn/parte2y4/EventoSalidaStockMinimo.scala
+++ ManejoStock/src/com/utn/parte2y4/EventoSalidaStockMinimo.scala
@@ -1,5 +1,20 @@
 package com.utn.parte2y4
+import com.utn.comun._
 
 class EventoSalidaStockMinimo extends ManejadorEventos {
+  
+	//RECIBO PRODUCTO, CANTENDEPOSITO, CANT A SACAR
+def manejar(producto: Producto, cantEnDeposito: Int, cant: Int){ 
+   if(cantEnDeposito < producto.cantMinima){
+     
+	   //GENERO PEDIDO DE COMPRA
+	   var pedidoCompra:PedidoDeCompra = new PedidoDeCompra(producto, producto.puntoDePedido)
+	   enviarACompras(pedidoCompra)
+   }
+ }
 
+def enviarACompras(pedido:PedidoDeCompra){
+  //FUERA DE NUESTRO ALCANCE
+}
+  
 }
--- ManejoStock/src/com/utn/parte2y4/EventoStockMinimo.scala
+++ ManejoStock/src/com/utn/parte2y4/EventoStockMinimo.scala
@@ -2,4 +2,7 @@ package com.utn.parte2y4
 
 class EventoStockMinimo extends ManejadorEventos {
 
+  def reservar(componente:String) = {
+		tipoReserva.reservar(componente, this)
+	}
 }
