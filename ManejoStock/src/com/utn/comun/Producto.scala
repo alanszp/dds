a628045375b60bc54e4c292ee6dcd704b86f2ac5
Uriel Petasny <petauriel@gmail.com>
2013-08-26 11:34:13 -0300

    Producto con los 3 valores

--- ManejoStock/src/com/utn/comun/Producto.scala
+++ ManejoStock/src/com/utn/comun/Producto.scala
@@ -3,7 +3,12 @@ package com.utn.comun
 import org.scalatest.junit._
 import org.junit.Assert._
 
-class Producto(override val nombre:String) extends ComponenteCompuesto(nombre) {
+class Producto(override val nombre:String, val cantMinima: Int,val cantMaxima : Int, val puntoDePedido : Int) extends ComponenteCompuesto(nombre) {
+  
+  //val cantMinima  : Int
+  
+ 
+
   
  override def assertClase {
     assertTrue("No es de clase producto", this.isInstanceOf[Producto])
