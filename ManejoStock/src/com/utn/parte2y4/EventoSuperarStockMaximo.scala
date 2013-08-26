6857183edf4b7e3891a141a2925d0b5ca4ae76c9
tomasgropper <tomasgropper@gmail.com>
2013-08-26 12:35:32 -0300

    Manejadores de eventos de entrada terminados. Falta el tema del log y tests

--- ManejoStock/src/com/utn/parte2y4/EventoSuperarStockMaximo.scala
+++ ManejoStock/src/com/utn/parte2y4/EventoSuperarStockMaximo.scala
@@ -1,5 +1,19 @@
 package com.utn.parte2y4
 
-class EventoSuperarStockMaximo extends ManejadorEventos {
+import com.utn.comun.Producto
 
+class EventoSuperarStockMaximo extends ManejadorEventos {
+    
+	  
+	def manejar(producto: Producto, cantEnDeposito: Int, cant: Int){
+	  
+	  //veo cuanto va a haber en el deposito luego del ingreso de productos
+	  val cantTotal = cantEnDeposito + cant;
+	  
+	  //si necesita log y supere el stock maximo lo logeo
+	  if(producto.necesitaLog && (cantTotal > producto.cantMaxima)){
+	    //IMPRIMO POR PANTALLA (VER COMO HACERLO DESP PARA LOS TEST)
+	  }
+	  
+	}
 }
--- ManejoStock/src/com/utn/parte2y4/EventoSuperarStockMinimo.scala
+++ ManejoStock/src/com/utn/parte2y4/EventoSuperarStockMinimo.scala
@@ -3,10 +3,16 @@ package com.utn.parte2y4
 import com.utn.comun.Producto
 
 class EventoSuperarStockMinimo extends ManejadorEventos {
-	def manejar(producto: Producto, cant: Int){
+    
 	  
-	  if(producto.necesitaLog){
-	    
+	def manejar(producto: Producto, cantEnDeposito: Int, cant: Int){
+	  
+	  //veo cuando falta en deposito para llegar al stock minimo
+	  val diferencia = producto.cantMinima - cantEnDeposito;
+	  
+	  //si necesita log y supere el stock minimo lo logeo
+	  if(producto.necesitaLog && (cant >= diferencia)){
+	    //IMPRIMO POR PANTALLA (VER COMO HACERLO DESP PARA LOS TEST)
 	  }
 	  
 	}
