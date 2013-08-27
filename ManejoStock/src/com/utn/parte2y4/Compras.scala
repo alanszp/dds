d4eefd499416103d03231d3ee64a40c6c30a7dd6
Uriel Petasny <petauriel@gmail.com>
2013-08-27 14:01:34 -0300

    Cambio clase Compras

--- ManejoStock/src/com/utn/parte2y4/Compras.scala
+++ ManejoStock/src/com/utn/parte2y4/Compras.scala
@@ -2,9 +2,9 @@ package com.utn.parte2y4
 
 import com.utn.comun.Producto
 
-class Compras(val producto: Producto, val cant:Int) {
+class Compras(val pedido: PedidoCompra) {
   
   //Fuera del alcance del ejercicio
-  def generarNotaDeCompra(prod: Producto, cant: Int) 
+  def generarNotaDeCompra(pedido:PedidoCompra) 
 
 }
