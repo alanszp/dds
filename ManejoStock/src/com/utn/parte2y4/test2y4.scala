857cdfe7ddc36e2f13e768f0c1695e5c641f198e
tomasgropper <tomasgropper@gmail.com>
2013-08-26 21:07:59 -0300

    Agrego el test2y4.scala para empezar a trabajar

---
+++ ManejoStock/src/com/utn/parte2y4/test2y4.scala
@@ -0,0 +1,15 @@
+package com.utn.parte2y4
+
+import org.scalatest.junit._
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.Before
+import org.junit.After
+import com.utn.comun._
+import scala.collection.immutable.Map
+import java.util.Dictionary
+import scala.collection.immutable.HashMap
+
+class test2y4 {
+
+}
