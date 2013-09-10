4d8647a36fa7c018c605d488ab1e5ccd8b398c2d
Uriel <petauriel@gmail.com>
2013-09-10 13:17:59 -0300

    Le agrego el metodo mostrar y comento lo que (aparentemente) no va

--- pruebaConcepto/src/main/scala/ar/edu/utn/dominio/Calculadora.scala
+++ pruebaConcepto/src/main/scala/ar/edu/utn/dominio/Calculadora.scala
@@ -6,53 +6,58 @@ import org.uqbar.commons.utils.Observable
 class Calculadora {
   var acumulador = 0.0
   var operando = 0.0
-  var display = 0.0
-  var firstTime = true
+ // var display = 0.0
+ // var firstTime = true
   
   
-  def checkAndSet(value:Double):Boolean = {
+ /* def checkAndSet(value:Double):Boolean = {
     if (firstTime) {
       acumulador = value
       firstTime = false
       return true
     }
     false
-  }
+  }*/
   
   def alFinal : Unit = {
-    operando = 0
+    operando = 0.0
   }
   
   
   def sumar = {
-    checkAndSet(0)
+    //checkAndSet(0)
     acumulador += operando
     alFinal
   }
   
   def restar = {
-    checkAndSet(0)
+    //checkAndSet(0)
     acumulador -= operando
     alFinal
   }
   
   def multiplicar = {
-    checkAndSet(1)
+    //checkAndSet(1)
     acumulador *= operando
     alFinal
   }
   
   def dividir = {
-    if (!checkAndSet(operando))
+    //if (!checkAndSet(operando))
+    if(operando != 0.0)
     	acumulador = acumulador / operando 
     alFinal
   }
-   
+  
   def reset = {
     acumulador = 0.0
     operando = 0.0
-    display = 0.0
-    firstTime = true
+    //display = 0.0
+    //firstTime = true
   }
-
+  
+  def mostrar = {
+    acumulador = value
+    
+  }
 }
