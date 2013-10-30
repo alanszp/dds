43750d61042b3377e8a6b4dfbd6b855e14bc436d
Tomas Gropper <tomasgropper@gmail.com>
2013-10-29 21:00:22 -0300

    Scripts terminados y andando

---
+++ Scripts TP 10/ScriptInsertDatos.sql
@@ -0,0 +1,54 @@
+
+INSERT INTO Cliente (id, nombre, apellido) VALUES (1, 'Tomas', 'Gropper')
+INSERT INTO Cliente (id, nombre, apellido) VALUES (2, 'Brian', 'Zuznicki')
+INSERT INTO Cliente (id, nombre, apellido) VALUES (3, 'Chuck', 'Espigiel')
+INSERT INTO Cliente (id, nombre, apellido) VALUES (4, 'Elizer', 'UriPeta')
+
+
+INSERT INTO Puesto_Venta (id, descripcion) VALUES (1, 'Abasto')
+INSERT INTO Puesto_Venta (id, descripcion) VALUES (2, 'Alto Palermo')
+
+INSERT INTO Noche (id, fecha) VALUES (1, '26/05/13')
+INSERT INTO Noche (id, fecha) VALUES (2, '27/10/13')
+
+INSERT INTO Descuento (id, precio) VALUES (1, 50)
+INSERT INTO Descuento (id, precio) VALUES (2, 100)
+
+INSERT INTO Descuento_Noche (noche, descuentos) VALUES (1, 1)
+INSERT INTO Descuento_Noche (noche, descuentos) VALUES (1, 2)
+INSERT INTO Descuento_Noche (noche, descuentos) VALUES (2, 1)
+
+INSERT INTO Categoria_Banda (id, tipo, precio_extra) VALUES (1, 'Nacional', 100)
+INSERT INTO Categoria_Banda (id, tipo, precio_extra) VALUES (2, 'Internacional poco conocido', 200)
+INSERT INTO Categoria_Banda (id, tipo, precio_extra) VALUES (3, 'Internacional', 500)
+
+INSERT INTO Banda (id, nombre, categoria) VALUES (1, 'Pimpinela', 1)
+INSERT INTO Banda (id, nombre, categoria) VALUES (2, 'Besruit', 1)
+INSERT INTO Banda (id, nombre, categoria) VALUES (3, 'Chayanne', 2)
+INSERT INTO Banda (id, nombre, categoria) VALUES (4, 'Rolling', 3)
+INSERT INTO Banda (id, nombre, categoria) VALUES (5, 'Aerosmith', 3)
+INSERT INTO Banda (id, nombre, categoria) VALUES (6, 'Bandana', 1)
+
+INSERT INTO Banda_Noche (noche, banda, comienzo, fin) VALUES (1, 1, 19, 22)
+INSERT INTO Banda_Noche (noche, banda, comienzo, fin) VALUES (2, 2, 19, 22)
+INSERT INTO Banda_Noche (noche, banda, comienzo, fin) VALUES (1, 3, 17, 19)
+INSERT INTO Banda_Noche (noche, banda, comienzo, fin) VALUES (2, 4, 17, 19)
+INSERT INTO Banda_Noche (noche, banda, comienzo, fin) VALUES (1, 5, 15, 17)
+INSERT INTO Banda_Noche (noche, banda, comienzo, fin) VALUES (2, 6, 15, 17)
+
+INSERT INTO Categoria_Persona (id, clase, precio_descuento, porcentaje_descuento) VALUES (1, 'Jubilados', 20, 20)
+INSERT INTO Categoria_Persona (id, clase, precio_descuento, porcentaje_descuento) VALUES (2, 'Menores', 10, 10)
+
+INSERT INTO Sector (id, nombre) VALUES (1, 'A')
+INSERT INTO Sector (id, nombre) VALUES (2, 'B')
+INSERT INTO Sector (id, nombre) VALUES (3, 'C')
+
+INSERT INTO Rango_Filas (id, desde, hasta, precio, cantAsientos, sector) VALUES (1, 1, 10, 500, 100, 1)
+INSERT INTO Rango_Filas (id, desde, hasta, precio, cantAsientos, sector) VALUES (2, 11, 20, 250, 100, 2)
+INSERT INTO Rango_Filas (id, desde, hasta, precio, cantAsientos, sector) VALUES (3, 21, 50, 50, 100, 3)
+
+INSERT INTO Entrada (id, sector, noche, categoria_persona, fila, asiento, cliente, puesto_venta) VALUES (1, 1, 1, 1, 1, 1, 1, 1)
+INSERT INTO Entrada (id, sector, noche, categoria_persona, fila, asiento, cliente, puesto_venta) VALUES (2, 2, 2, 2, 2, 2, 2, 2)
+
+
+
---
+++ Scripts TP 10/SriptCreacional.sql
@@ -0,0 +1,100 @@
+CREATE TABLE Puesto_Venta (
+        id int,
+        descripcion varchar(30),
+        PRIMARY KEY (id)
+)
+
+CREATE TABLE Cliente (
+        id int,
+        nombre varchar(30),
+        apellido varchar(30),
+        PRIMARY KEY (id)
+)
+
+
+CREATE TABLE Sector(
+        id int,
+        nombre varchar(30),
+        PRIMARY KEY (id)
+)
+
+CREATE TABLE Rango_Filas(
+        id int,
+        desde int,
+        hasta int,
+        precio int,
+        cantAsientos int,
+        sector int,
+        PRIMARY KEY (id),
+        FOREIGN KEY (sector) REFERENCES Sector(id)
+)
+
+CREATE TABLE Categoria_Persona(
+        id int,
+        clase varchar(30),
+        precio_descuento int,
+        porcentaje_descuento int,
+        PRIMARY KEY (id)
+)
+
+CREATE TABLE Noche(
+        id int,
+        fecha datetime,
+        PRIMARY KEY (id)
+)
+
+CREATE TABLE Categoria_Banda(
+        id int,
+        tipo varchar(30),
+        precio_extra int,
+        PRIMARY KEY (id)
+)
+
+CREATE TABLE Banda(
+        id int,
+        nombre varchar(30),
+        categoria int,
+        PRIMARY KEY (id),
+        FOREIGN KEY (categoria) REFERENCES Categoria_Banda(id)
+)
+
+CREATE TABLE Banda_Noche(
+        noche int NOT NULL,
+        banda int NOT NULL,
+        comienzo int,
+        fin int,
+        FOREIGN KEY (noche) REFERENCES Noche(id),
+        FOREIGN KEY (banda) REFERENCES Banda(id)
+)
+
+CREATE TABLE Descuento(
+		id int,
+		precio int,
+		PRIMARY KEY (id)
+)
+
+CREATE TABLE Descuento_Noche(
+        noche int NOT NULL,
+        descuentos int NOT NULL,
+        FOREIGN KEY (noche) REFERENCES Noche(id),
+        FOREIGN KEY (descuentos) REFERENCES Descuento(id)
+)
+
+
+CREATE TABLE Entrada(
+        id int,
+        sector int,
+        noche int,
+        categoria_persona int,
+        fila int,
+        asiento int,
+        cliente int,
+        puesto_venta int,
+        PRIMARY KEY (id),
+        FOREIGN KEY (sector) REFERENCES Sector(id),
+        FOREIGN KEY (noche) REFERENCES Noche(id),
+        FOREIGN KEY (categoria_persona) REFERENCES Categoria_Persona(id),
+        FOREIGN KEY (cliente) REFERENCES Cliente(id),
+        FOREIGN KEY (puesto_venta) REFERENCES Puesto_Venta(id)
+
+)
