04069e07b4a86b0fbeaa57091ae61e0ebba0595f
gkuz <guidokuz@gmail.com>
2013-10-29 22:40:22 -0300

    mi consulta locoooo

--- TP 10/Consultas.sql
+++ TP 10/Consultas.sql
@@ -24,3 +24,15 @@ FROM Banda B
 JOIN Banda_Noche BN  ON N.id = BN.noche
 JOIN Noche N ON B.id = BN.banda
 WHERE B.nombre LIKE 'Catupecu Machu'
+
+--Integrante 4: Guido Kuznicki
+
+SELECT N.Fecha as Noche, B.Nombre as Nombre_Banda, C.Tipo as Categoria_Banda, C.PrecioExtra as Precio_Extra_Banda, BN.Comienzo as Hora_Comienzo, BN.Fin as Hora_Fin 
+FROM Banda B 
+	JOIN CategoriaBanda C ON C.ID = B.Categoria
+	JOIN Banda_Noche BN on BN.Banda = B.ID
+	JOIN Noche N ON N.ID = BN.Noche
+WHERE N.Fecha="23/12/2013"
+
+
+
