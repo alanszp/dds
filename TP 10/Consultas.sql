13243003dfbe20e70b190b0749bc5269a4bac35a
Uriel <petauriel@gmail.com>
2013-10-29 22:18:58 -0200

    Subo mi Query
    Subo mi Query

--- TP 10/Consultas.sql
+++ TP 10/Consultas.sql
@@ -1,3 +1,14 @@
+--Integrante 1 : Uriel Petasny
+
+SELECT s.nombre, n.fecha, fila, asiento, c.nombre, c.apellido
+FROM Entrada e
+	JOIN Cliente c ON e.cliente = c.id
+	JOIN Noche n ON e.noche = n.id
+	JOIN Sector s ON e.sector = s.id
+WHERE c.nombre = 'Brian' AND c.apellido = 'Zuznicki'
+
+--Integrante 2: Tomas Gropper
+
 SELECT N.fecha, S.nombre, CP.clase, C.nombre + ' ' + C.apellido AS Cliente, P.descripcion AS PuestoVenta
 FROM Noche N JOIN Entrada E ON E.noche = N.id
 			JOIN Sector S ON E.sector = S.id
