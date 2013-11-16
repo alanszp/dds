b3570203a3dcaad71149d3af36aba5156bf8975c
Tomas Gropper <tomasgropper@gmail.com>
2013-11-16 18:16:09 -0300

    definiendo los homes

--- tp-arena-persistence-tp-11/src/main/scala/home/HomeNoches.scala
+++ tp-arena-persistence-tp-11/src/main/scala/home/HomeNoches.scala
@@ -6,8 +6,12 @@ import scala.collection.JavaConverters._
 import scala.collection.JavaConversions._
 import uqbar.arena.persistence.PersistentHome
 import scala.collection.JavaConverters._
+import recital._
 
 @Observable
-class HomeNoches extends PersistentHome<Noche> implements Serializable {
-
+class HomeNoches extends PersistentHome[Noche] with Serializable {
+  
+  override def getEntityType(): Class[Noche] = ???
+  override def createExample(): Noche = ???
+  
 }
