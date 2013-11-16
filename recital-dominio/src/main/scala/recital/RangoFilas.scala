bd482a03e45b128fd1fd1ec7a57b6db5624d0729
Tomas Gropper <tomasgropper@gmail.com>
2013-11-16 16:55:23 -0300

    mas clases preparadas

--- recital-dominio/src/main/scala/recital/RangoFilas.scala
+++ recital-dominio/src/main/scala/recital/RangoFilas.scala
@@ -1,5 +1,7 @@
 package recital
 
+import uqbar.arena.persistence.annotations.PersistentField
+
 class RangoFilas extends Serializable {
   
   def this(desde: Int, hasta: Int, precio: Double, cantAsientos:Int) = {
@@ -19,4 +21,20 @@ class RangoFilas extends Serializable {
   def tengoFila(fila: Int): Boolean = (this.desde <= fila) && (this.hasta >= fila)
    
   def existeAsiento(asiento: Int):Boolean = asiento <= this.cantAsientos && asiento > 0
+  
+  	@PersistentField
+	def getprecio = precio
+	def setprecio(n:Double) = precio = n
+	
+	@PersistentField
+	def getdesde = desde
+	def setdesde(n:Int) = desde = n
+	
+	@PersistentField
+	def gethasta = hasta
+	def sethasta(n:Int) = hasta = n
+	
+	@PersistentField
+	def getcantAsientos = cantAsientos
+	def setcantAsientos(n:Int) = cantAsientos = n
 }
