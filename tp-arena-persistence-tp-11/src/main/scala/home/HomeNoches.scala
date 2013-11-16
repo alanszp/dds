dae86f0c60a412e82e7d1776a75eb42cfef7e9ba
Tomas Gropper <tomasgropper@gmail.com>
2013-11-16 18:45:56 -0300

    metodo create en homeNoche

--- tp-arena-persistence-tp-11/src/main/scala/home/HomeNoches.scala
+++ tp-arena-persistence-tp-11/src/main/scala/home/HomeNoches.scala
@@ -1,9 +1,11 @@
 package home
 
 import org.uqbar.commons.utils.Observable
-
 import recital.Noche
 import uqbar.arena.persistence.PersistentHome
+import recital.Banda
+import recital.CategoriaPersona
+import recital.Sector
 
 @Observable
 object HomeNoches extends PersistentHome[Noche] with Serializable {
@@ -11,4 +13,12 @@ object HomeNoches extends PersistentHome[Noche] with Serializable {
   override def getEntityType(): Class[Noche] = classOf[Noche]
   override def createExample(): Noche = new Noche
   
+  def create(pNombreNoche: String, pFecha: String, pBandas: Set[Banda], pdescuentos: Set[CategoriaPersona], pSectores: Set[Sector]): Unit = {
+  var noche = new Noche
+  noche.setNombreNoche(pNombreNoche)
+  noche.setBandas(pBandas)
+  noche.setDescuentos(pdescuentos)
+  noche.setSectores(pSectores)
+  this.create(noche)
+  } 
 }
