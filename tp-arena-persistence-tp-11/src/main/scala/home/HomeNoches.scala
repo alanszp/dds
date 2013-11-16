d794e1e7c9e60e6fa5905205d92417145062e7e1
Tomas Gropper <tomasgropper@gmail.com>
2013-11-16 18:32:17 -0300

    agregado el configure al runner

--- tp-arena-persistence-tp-11/src/main/scala/home/HomeNoches.scala
+++ tp-arena-persistence-tp-11/src/main/scala/home/HomeNoches.scala
@@ -1,12 +1,9 @@
 package home
 
-import org.uqbar.commons.model.CollectionBasedHome
 import org.uqbar.commons.utils.Observable
-import scala.collection.JavaConverters._
-import scala.collection.JavaConversions._
+
+import recital.Noche
 import uqbar.arena.persistence.PersistentHome
-import scala.collection.JavaConverters._
-import recital._
 
 @Observable
 class HomeNoches extends PersistentHome[Noche] with Serializable {
--- tp-arena-persistence-tp-11/src/main/scala/runner/Runner.scala
+++ tp-arena-persistence-tp-11/src/main/scala/runner/Runner.scala
@@ -2,8 +2,12 @@ package runner
 
 import view.ListadorEntradaWindow
 import org.uqbar.arena.Application
+import uqbar.arena.persistence.Configuration
 
 object Runner extends Application with App {
 	  def createMainWindow() = new ListadorEntradaWindow(this)
+	  // Arena-persistence levanta los objetos persistidos
+	  Configuration.configure()
+	 // Fin Arena-persistence
 	  start()
 }
