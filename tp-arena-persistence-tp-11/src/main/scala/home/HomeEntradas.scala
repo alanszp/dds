9fbb54ed733266b310af609e47c51c71b0381fa7
Uri <petauriel@gmail.com>
2013-11-16 19:05:55 -0300

    Agrego el metodo create

--- tp-arena-persistence-tp-11/src/main/scala/home/HomeEntradas.scala
+++ tp-arena-persistence-tp-11/src/main/scala/home/HomeEntradas.scala
@@ -3,6 +3,10 @@ package home
 import org.uqbar.commons.utils.Observable
 
 import recital.Entrada
+import recital.Banda
+import recital.CategoriaPersona
+import recital.Noche
+import recital.Sector
 import uqbar.arena.persistence.PersistentHome
 
 @Observable
@@ -10,5 +14,17 @@ object HomeEntradas extends PersistentHome[Entrada] with Serializable {
   
   override def getEntityType(): Class[Entrada] = classOf[Entrada]
   override def createExample(): Entrada = new Entrada
+  
+  def create(pSector: Sector, pNoche: Noche, pCategoria:CategoriaPersona, pFila: Int, pAsiento:Int, pCliente:String, pPuestoVenta:String): Unit = {
+  var entrada = new Entrada
+  entrada.setPuestoVenta(pPuestoVenta)
+  entrada.setCliente(pCliente)
+  entrada.setFila(pFila)
+  entrada.setCategoriaPersona(pCategoria)
+  entrada.setNoche(pNoche)
+  entrada.setSector(pSector)
+  entrada.setAsiento(pFila)
+  this.create(entrada)
+  } 
   
 }
