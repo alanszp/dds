b4aafc624c0aebb9ebfdafc047676264de7b0ded
Tomas Gropper <tomasgropper@gmail.com>
2013-11-18 11:52:05 -0300

    agregue persistentclass a catpersona

--- recital-dominio/src/main/scala/recital/CategoriaPersona.scala
+++ recital-dominio/src/main/scala/recital/CategoriaPersona.scala
@@ -2,7 +2,9 @@ package recital
 
 import org.uqbar.commons.model.Entity
 import compatibility.DomainEntity
+import uqbar.arena.persistence.annotations.PersistentClass
 
+@PersistentClass
 abstract class CategoriaPersona extends DomainEntity {
   val descripcion :String
   def aplicarDescuento(EntradaBase: Double):Double
