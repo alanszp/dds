512dff5f56a3979d04cc6bf97113d0b0c87acf6b
Uri <petauriel@gmail.com>
2013-11-18 11:10:59 -0300

    getters y setters

--- recital-dominio/src/main/scala/recital/Damas.scala
+++ recital-dominio/src/main/scala/recital/Damas.scala
@@ -17,8 +17,9 @@ class Damas extends CategoriaPersona {
     EntradaBase * this.porcentajeDescuento
   }
   
+  	def setporcentajeDescuento(n:Double) = porcentajeDescuento = n
   	@PersistentField
 	def getporcentajeDescuento = porcentajeDescuento
-	def setporcentajeDescuento(n:Double) = porcentajeDescuento = n
+
 }
 
