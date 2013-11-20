597cf41c41100d5505a40773d16ba5478d8a9cea
Alan Szpigiel <alanszp@gmail.com>
2013-11-20 10:00:11 -0300

    Revert "mas arreglos (creo jaja)"
    This reverts commit b29d44d0f381a5a75d69f62ec80ccfa8253f1781.
    
    Conflicts:
    	recital-dominio/src/main/scala/recital/Mayor.scala

--- recital-dominio/src/main/scala/recital/Mayor.scala
+++ recital-dominio/src/main/scala/recital/Mayor.scala
@@ -2,17 +2,11 @@ package recital
 
 import uqbar.arena.persistence.annotations.PersistentClass
 
-@PersistentClass
+
 class Mayor extends CategoriaPersona {
   
    val _descripcion = "Mayor"
    override def aplicarDescuento(EntradaBase: Double):Double = {
      0
    }
-<<<<<<< HEAD
-=======
-   
-   @PersistentField
-   def descripcion = _descripcion
->>>>>>> 14d63d55bbb047955691907cb197d92b648481fb
 }
